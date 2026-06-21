049e709e108e50e15f837543f3aae20aa36121fc
Rhiki <rhiki.080bct033@tcioe.edu.np>
2026-06-21 16:57:23 +0545

    Update vitest.config.ts

--- src/vitest.config.ts
+++ src/vitest.config.ts
@@ -1,11 +1,1 @@
-import {config} from 'dotenv'
-import {defineConfig} from 'vitest/config'
 
-
-config ({path :'.env.local'})
-export default defineConfig({
-  test: {
-    environment: 'node',
-    globals: true,
-  },
-})
